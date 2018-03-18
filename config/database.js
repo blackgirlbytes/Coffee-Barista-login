ca9856367905055dd090e3a592b00df3dabd3dd4
RizelBobb <rizel.bobbsemple.137@gmail.com>
2018-03-18 18:39:45 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://rizel:bobb@ds161099.mlab.com:61099/coffee' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
